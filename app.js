8180c797839488b976334ff34897ebe71a72d789
CaioPedras <caio.vp@gmail.com>
2022-07-04 12:20:23 -0300

    teste

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import express, { json } from "express";
 import dotenv from "dotenv";
 import authRouter from "./Routers/AuthRouter.js";
-import transactionRouter from "./Routers/TransactionsRouter.js";
+import transactionRouter from "./Routers/transactionsRouter.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(json());
 
 app.use(authRouter);
+
 app.use(transactionRouter);
 
 app.listen(process.env.PORT);
